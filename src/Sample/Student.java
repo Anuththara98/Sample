9117030d243ef3adcbe008addd4934721da1e666
anuththara kavindi <anukavindi@gmail.com>
2019-08-03 14:10:48 +0530

    initial commit

---
+++ src/Sample/Student.java
@@ -0,0 +1,21 @@
+package Sample;
+
+public class Student {
+	private int sid;
+	private int sname;
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public int getSname() {
+		return sname;
+	}
+	public void setSname(int sname) {
+		this.sname = sname;
+	}
+	
+	
+
+}
